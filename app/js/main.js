8af243a370177f20ba8319a11112ce55106ad68f
Timur525 <7152521@gmail.com>
2019-11-27 15:36:29 +1000

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "13px",
+    readOnly: true,
+  });
+
+  $(".product-slider__inner").slick({
+    slidesToScroll: 4,
+    slidesToShow: 4,
+    arrows: false,
+    dots: true,
+
+  });
+
   var mixer = mixitup('.products__inner-box')
 
 });
