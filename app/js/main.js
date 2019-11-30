bace4e6090d680f91ddd361c5de5cebef63c2191
Timur525 <7152521@gmail.com>
2019-11-30 16:18:11 +1000

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,15 @@ $(function(){
     slidesToShow: 4,
     arrows: false,
     dots: true,
+  });
 
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
   });
 
   var mixer = mixitup('.products__inner-box')
